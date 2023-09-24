7d40a8d8e651a2b16f00b7ff0bd49cb07f20f0c9
kerbaudisko21 <wilbertw99@gmail.com>
2023-09-24 15:42:42 +0700

    first commit

---
+++ app/(root)/page.tsx
@@ -0,0 +1,35 @@
+import ThreadCard from "@/components/cards/ThreadCard";
+import { fetchPost } from "@/lib/actions/thread.action";
+import { UserButton } from "@clerk/nextjs";
+import { currentUser } from "@clerk/nextjs";
+ 
+export default async function Home() {
+  const result = await fetchPost(1, 30);
+  const user = await currentUser();
+  
+  return (
+    <>
+      <section className="mt-9 flex flex-col gap-10">
+        {result.posts.length === 0 ? (
+          <p className="no-result">No Threads found</p>
+        ) : (
+          <>
+            {result.posts.map((post) => (
+              <ThreadCard
+                key = {post._id}
+                id = {post._id}
+                currentUserId = {user?.id || ""}
+                parentId = {post.parentId}
+                content = {post.text}
+                author = {post.author}
+                community = {post.community}
+                createdAt = {post.createdAt}
+                comments = {post.children}
+              />
+            ))}
+          </>
+        )}
+      </section>
+    </>
+  )
+}
